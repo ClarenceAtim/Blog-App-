fef3f2fe7afa76ad462c042a3599865d64b76b98
clarence <tegumclarence@gmail.com>
2023-09-30 09:02:11 +0100

    Crud d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,21 +1,22 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments
   has_many :likes
+  belongs_to :author, class_name: 'User'
 
-  validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  attribute :title, :string
+  attribute :text, :text
+  attribute :comments_counter, :integer, default: 0
+  attribute :likes_counter, :integer, default: 0
 
-  after_save :update_author_counter
-
-  def recent_comments(limit = 5)
-    comments.order(created_at: :desc).limit(limit)
-  end
+  after_save :update_post_counter
 
   private
 
-  def update_author_counter
+  def update_post_counter
     author.increment!(:posts_counter)
+  end
+
+  def five_most_recent_comments
+    comments.order(created_at: :desc).limit(5)
   end
 end
