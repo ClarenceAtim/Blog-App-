84d2e58de4a53758acabc30cd46b7615ba4110ae
clarence <tegumclarence@gmail.com>
2023-09-30 08:43:14 +0100

    Setting up rs enviroment

---
+++ app/models/post.rb
@@ -0,0 +1,21 @@
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments
+  has_many :likes
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  after_save :update_author_counter
+
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
+
+  private
+
+  def update_author_counter
+    author.increment!(:posts_counter)
+  end
+end
